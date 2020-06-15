53f0a56a2627a5696fa09424b4a76205fd7f4b45
Arsi Ikäheimonen <arsi.ikaheimonen@gmail.com>
2020-06-15 14:52:44 +0300

    fixed some nonsense

--- json_load.py
+++ json_load.py
@@ -4,26 +4,18 @@ Created on Fri Jun  5 15:06:32 2020
 
 @author: arsii
 
-<<<<<<< HEAD
 Load all .json files in a given folder. The files are stored in a dictinary, 
 where file names are used as keys. All the subfolders in the given folder 
 are ignored. If there is file of a type other than .json in the folder, 
 an exception is raised.
-=======
-Load all .json files in a given folder. The files are stored in a list.
-All the subfolders in the given folder are ignored. If there is file of 
-a type other than .json in the folder, an exception is raised.
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
+
 
 """
 
 from __future__ import print_function
 import json
 from pathlib import Path
-<<<<<<< HEAD
-=======
 import itertools
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
 from os import listdir
 from os.path import isfile, join, exists
     
@@ -33,38 +25,32 @@ def load_one_subject(open_name):
 
     Parameters
     ----------
-<<<<<<< HEAD
     openname : Path -object
-=======
-    openname : WindowsPath 
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
         path to the .json file
 
     Returns
     -------
-<<<<<<< HEAD
     file_name : str
         Name of the given file without suffix
-=======
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
+
+
     json_file: dict
         a dictionary based on the read json file
 
     """
-    
-<<<<<<< HEAD
+
     with open_name.open('r') as read_file:
         json_file = json.load(read_file)
         file_name = open_name.stem
         return file_name, json_file
-=======
+
     try:
         with open(str(open_name), "r") as read_file:
             json_file = json.load(read_file)
             return json_file
     except IOError:
         print("Cannot open the file.")
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
+
     
 def load_all_subjects(foldername):
     """
@@ -72,29 +58,26 @@ def load_all_subjects(foldername):
 
     Parameters
     ----------
-<<<<<<< HEAD
+
     foldername : Path -object
-=======
-    foldername : WindowsPath 
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
         path to the folder containing .json files
 
     Returns
     -------
-<<<<<<< HEAD
+
     json_dict: dict
         a dictionary containing readed .json files, 
         filenames used as dict keys.
-=======
+
     json_list: list 
         a list containing readed .json files
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
+
 
     """
     
     if exists(foldername):
         file_list = [f for f in listdir(foldername) if isfile(join(foldername, f))]
-<<<<<<< HEAD
+
     else:
         raise Exception("Requested folder: " + str(foldername) + " does not exist.") 
     
@@ -108,8 +91,7 @@ def load_all_subjects(foldername):
          
     if json_dict:
         return json_dict
-=======
-        #file_list = listdir(foldername)
+
     else:
         raise Exception("Requested folder: " + str(foldername) + " does not exist.") 
     
@@ -122,20 +104,17 @@ def load_all_subjects(foldername):
          
     if json_list:
         return json_list
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
+    
     else:
         raise Exception("There is no files in the selected folder.")
 
 
 if __name__ == "__main__":
     # give the correct folder here
-<<<<<<< HEAD
+
     DATA_FOLDER = Path(r'C:/Users/arsii/Documents/Work/Json_test/files/')
     json_dict = load_all_subjects(DATA_FOLDER)
-    
-    
-=======
     DATA_FOLDER = Path("G:/SpecialAssignment/Json_test/files/")
     json_list = load_all_subjects(DATA_FOLDER)
     json_list = list(itertools.chain.from_iterable(json_list))
->>>>>>> 80cfa83ecf66c9904bf8fcf4ca44d6290ef33627
+
