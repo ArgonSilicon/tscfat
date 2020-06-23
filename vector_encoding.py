49837363877e54440a74dc8e4d1ffa5cd4714b8b
Arsi Ikäheimonen <arsi.ikaheimonen@gmail.com>
2020-06-23 15:33:31 +0300

    added decoding functions

--- vector_encoding.py
+++ vector_encoding.py
@@ -18,3 +18,20 @@ def ordinal_encoding(timeseries):
     enc.fit(timeseries)
     transformed = enc.transform(timeseries)
     return transformed
+
+def decode_string(cell_value):
+    cell_value = str(cell_value)
+    if (cell_value == 0 or cell_value.lower() == "nan" or cell_value.lower() == "no"):
+        ret = int(0)
+    else:
+        ret = int(1)
+    return ret
+
+def decode_string_3(cell_value):
+    splitted = cell_value.split()
+    cleaned = [''.join(filter(str.isalnum, s)).lower() for s in splitted]
+    ret = cleaned.count('yes')   
+    return ret  
+
+def custom_resampler(array_like):
+    return array_like.values               
