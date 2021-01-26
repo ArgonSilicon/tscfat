52e8cdb5808bd2858698cf61948c7d2aedc71854
ArgonSilicon <arsi.ikaheimonen@gmail.com>
2021-01-26 16:27:03 +0200

    26012021

--- Source/studentlife.py
+++ Source/studentlife.py
@@ -37,6 +37,12 @@ def main():
     #%%
     DATA_FOLDER = Path(r'F:\StudentLife\dataset\sensing\activity')
     
+    first = None
+    last = None
+    st1 = pd.Timestamp('2013-03-27 04:00:00')
+    st2 = pd.Timestamp('2013-06-01 04:00:00')
+    ix = pd.date_range(start=st1, end=st2, freq='H')
+    
     for file in os.listdir(DATA_FOLDER):
         print(file)
         subject = os.path.join(DATA_FOLDER, file)
@@ -47,14 +53,47 @@ def main():
             df['timestamp'] = pd.to_datetime(df['timestamp'],unit='s',origin='unix')
             df = df.set_index('timestamp')
             df.columns = ['activity']
-            df = df[df['activity'] > 0]
             
-            resampled = df.resample("H").count()
+            '''
+            start = df.index[0]
+            end = df.index[-1]
+            
+            if not first:
+                first = start
+            if not last:
+                last = end
+            if end > last:
+                last == end
+            if start < first:
+                first = start
+            '''
+            
+            df_active = df[df['activity'] > 0] # subject is active
+            resampled_active = df_active.resample("H").count()
+            resampled_active = resampled_active.reindex(ix)
+            
+            # fo missing data
+            resampled = df.resample("H").sum(min_count=1)
+            resampled = resampled.reindex(ix) 
+            
+            missing_values = resampled.isna().astype(int)
+            missing_values.plot(title="Missing datapoints / hourly")
+            print(missing_values.shape)
+            
+            m_day = missing_values.resample("D").sum()
+            m_day['time'] = m_day.index
+            m_day['time'] = pd.to_datetime(m_day['time'])
+            m_day = m_day.groupby(m_day['time'].dt.day_name()).sum()
+            m_day.index = pd.Categorical(m_day.index, categories=
+                                           ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday'],
+                                           ordered=True)
+            m_day = m_day.sort_index()
+            m_day.plot(kind='bar',title="Missing datapoints",ylabel='Count')
             
             # create a scaler object
             scaler = MinMaxScaler()
             # fit and transform the data
-            df_norm = scaler.fit(resampled['activity'].values.reshape(-1,1))
+            df_norm = scaler.fit(resampled_active['activity'].values.reshape(-1,1))
             resampled['activity'] = df_norm.transform(resampled['activity'].values.reshape(-1,1))
         
         
@@ -62,9 +101,13 @@ def main():
             clusters = process_activity(resampled,FIGPATH)
         except:
             print("Something went horribly wrong!")
+            
+    #%%
+    rng=pd.date_range(start=df.index.min(), periods=35, freq='H')
+    df.reindex(rng).ffill()
     #%%
     DATA_FOLDER = Path(r'F:\StudentLife\dataset\EMA\response\Behavior')
-    subject = DATA_FOLDER / 'Behavior_u00.json'
+    subject = DATA_FOLDER / 'Behavior_u04.json'
     df = pd.read_json(subject)
     
     #%%
@@ -92,13 +135,14 @@ def main():
         
         try:
             df = pd.read_json(current_file)
-            df_filt = df.filter(["resp_time","level",])
+            df_filt = df.filter(["resp_time","happy",])
             df_filt = df_filt.set_index('resp_time')
             resampled = df_filt.resample("D").mean()
             resampled.plot()
         except:
             print('Something fishy here')
-            
+
+    #%%            
 if __name__ == '__main__':
 
     main()
