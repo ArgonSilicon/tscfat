a8b67835a14763bd7c7afe2fee014074956669e2
ArgonSilicon <arsi.ikaheimonen@gmail.com>
2021-03-25 12:39:47 +0200

    fixed tests

--- Source/Utils/argument_loader.py
+++ Source/Utils/argument_loader.py
@@ -15,32 +15,35 @@ def setup_np():
     #print(Path.cwd())
     #open_name =  Path(r'F:\tscfat\Data\Battery_test_data_1.csv')
     #open_name = Path('/u/26/ikaheia1/data/Documents/SpecialAssignment/tscfat/Data/Battery_test_data_1.csv')
-    open_name = Path.cwd() / 'Data' / 'Battery_test_data_1.csv'
+    open_name = Path.cwd() / 'Data' / 'Test_data.csv'
     with open_name.open('r') as read_file:
-        df = pd.read_csv(read_file)
-        df['time'] = pd.to_datetime(df['time'],unit = 's')
-        return df['battery_level'].values
+        df = pd.read_csv(read_file, index_col=0)
+        df['time'] = pd.to_datetime(df['time'])
+        df = df.set_index('time')
+        return df['level'].values
     
 
 def setup_ps():
 
     #print(Path.cwd())
     # open_name =  Path(r'F:\tscfat\Data\Battery_test_data_1.csv')
-    open_name = Path.cwd() / 'Data' / 'Battery_test_data_1.csv'
+    open_name = Path.cwd() / 'Data' / 'Test_data.csv'
     #open_name = Path('/u/26/ikaheia1/data/Documents/SpecialAssignment/tscfat/Data/Battery_test_data_1.csv')
     with open_name.open('r') as read_file:
-        df = pd.read_csv(read_file)
-        df['time'] = pd.to_datetime(df['time'],unit = 's')
-        return df['battery_level']
+        df = pd.read_csv(read_file, index_col=0)
+        df['time'] = pd.to_datetime(df['time'])
+        df = df.set_index('time')
+        return df['level']
   
 def setup_pd():
     
     #print(Path.cwd())
-    open_name = Path.cwd() / 'Data' / 'Battery_test_data_1.csv'
+    open_name = Path.cwd() / 'Data' / 'Test_data.csv'
     #open_name =  Path(r'F:\tscfat\Data\Battery_test_data_1.csv')
     #open_name =  Path(r'C:\Users\arsii\Documents\tscfat\Data\Battery_test_data_1.csv')
     #open_name = Path('/u/26/ikaheia1/data/Documents/SpecialAssignment/tscfat/Data/Battery_test_data_1.csv')
     with open_name.open('r') as read_file:
-        df = pd.read_csv(read_file)
-        df['time'] = pd.to_datetime(df['time'],unit = 's')
+        df = pd.read_csv(read_file, index_col=0)
+        df['time'] = pd.to_datetime(df['time'])
+        df = df.set_index('time')
         return df
