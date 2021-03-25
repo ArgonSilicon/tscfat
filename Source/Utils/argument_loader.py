95510e425db2d2aceacf3c1dae4a585212061287
ArgonSilicon <arsi.ikaheimonen@gmail.com>
2021-03-25 14:00:31 +0200

    added example data

--- Source/Utils/argument_loader.py
+++ Source/Utils/argument_loader.py
@@ -6,13 +6,14 @@ Created on Fri Nov 20 14:31:11 2020
 
 @author: ikaheia1
 """
-
+import os
 import pandas as pd
 from pathlib import Path
 
 def setup_np():
     
     open_name = Path.cwd() / 'Data' / 'Test_data.csv'
+    
     with open_name.open('r') as read_file:
         df = pd.read_csv(read_file, index_col=0)
         df['time'] = pd.to_datetime(df['time'])
@@ -23,6 +24,7 @@ def setup_np():
 def setup_ps():
 
     open_name = Path.cwd() / 'Data' / 'Test_data.csv'
+
     with open_name.open('r') as read_file:
         df = pd.read_csv(read_file, index_col=0)
         df['time'] = pd.to_datetime(df['time'])
@@ -32,6 +34,7 @@ def setup_ps():
 def setup_pd():
     
     open_name = Path.cwd() / 'Data' / 'Test_data.csv'
+
     with open_name.open('r') as read_file:
         df = pd.read_csv(read_file, index_col=0)
         df['time'] = pd.to_datetime(df['time'])
