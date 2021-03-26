a0e3f62cb66515c9ec23d94b569b002007109e16
Arsi Ikäheimonen <61492903+ArgonSilicon@users.noreply.github.com>
2021-03-26 13:18:05 +0200

    Update setup.py
    setup file for pypi release

--- setup.py
+++ setup.py
@@ -5,36 +5,36 @@ Created on Mon Jan 18 14:06:42 2021
 
 @author: arsii
 """
-"""
-from setuptools import setup, find_packages
 
-setup(name="tscfat",
-      packages = find_packages())
-"""
 from distutils.core import setup
+
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  name = 'tscfat',         
+  packages = ['tscfat'],   
+  version = '0.0.1',      
+  license='MIT',        
+  description = 'A time series co-fluctuation analysis toolbox',   
+  author = 'Arsi Ikaheimonen',                   
+  author_email = 'arsi.ikaheimonen@gmail.com',      
+  url = 'https://github.com/ArgonSilicon/tscfat',   
+  download_url = 'https://github.com/ArgonSilicon/tscfat/archive/v_01.tar.gz',    
+  keywords = ['Timeseries', 'Exploratory', 'Analysis'],  
+  install_requires=['nolds',
+                    'sklearn',
+                    'pytest',
+                    'numpy',
+                    'matplotlib',
+                    'pandas',
+                    'statsmodels',
+                    'tslearn'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Researchers',      
+    'Topic :: Mood research :: Time series analysis', #?
+    'License :: OSI Approved :: MIT License',   #P
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
