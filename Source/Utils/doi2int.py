2efa180fc1cc20ac168bbe3ee313919443e7d835
ArgonSilicon <arsi.ikaheimonen@gmail.com>
2021-03-30 16:03:02 +0300

    update doi2int

--- Source/Utils/doi2int.py
+++ Source/Utils/doi2int.py
@@ -4,68 +4,81 @@
 Created on Tue Mar 23 11:46:24 2021
 
 @author: ikaheia1
+
+A helper function for converting the datetime to indices used for plotting.
 """
+import pandas as pd
 from datetime import datetime
 from matplotlib.dates import date2num
 
 def _doi2int(doi):
     """
+    Convert tuple of two datetime objects to Matplotlib dates. 
     
 
     Parameters
     ----------
-    doi : TYPE
-        DESCRIPTION.
+    doi : tuple
+        Tuple contains two datetime objects.
 
     Returns
     -------
-    indices : TYPE
-        DESCRIPTION.
+    indices : float
+        Number of days since the unix time epoch.
 
     """
+    assert isinstance(doi, tuple), "Doi is not a tuple."
     
     indices = (date2num(datetime(*doi[0])),date2num(datetime(*doi[1])))
     return indices
 
 def _ts2int(ts):
     """
-    
+    Convert a datetime objects to Matplotlib date.
 
     Parameters
     ----------
-    ts : TYPE
-        DESCRIPTION.
+    ts : datetime.datetime or numpy.datetime64
+        A datetime object.
 
     Returns
     -------
-    index : TYPE
-        DESCRIPTION.
+    index : float
+        Number of days since the unix time epoch.
 
     """
+    assert isinstance(ts, pd._libs.tslibs.timestamps.Timestamp), "Timestamp is not in datetime format"
+   
     index = date2num(ts)
     return index
 
 def doi2index(doi,df):
-    """
+    """ 
+    Convert given range given in datetime objects into corresponding 
+    Matplotlib indices, that are used to highligh the region in analysis 
+    figures. 
     
 
     Parameters
     ----------
-    doi : TYPE
-        DESCRIPTION.
-    df : TYPE
-        DESCRIPTION.
+    doi : tuple
+        Contain two datetime objects.
+    df : pandas DataFrame
+        A dataframe with datetime index.
 
     Returns
     -------
-    ind_s : TYPE
-        DESCRIPTION.
-    ind_e : TYPE
-        DESCRIPTION.
+    ind_s : float
+        Starting index. Number of days since the unix time epoch.
+    ind_e : float
+        End index. Number of days since the unix time epoch.
 
     """
     
-    #TODO! insert asserts here
+    assert isinstance(doi, tuple), "Doi is not a tuple."
+    assert isinstance(doi[0], tuple), "The first item in tuple is not a tuple."
+    assert isinstance(doi[1], tuple), "The second item in tuple is not a tuple."
+    assert isinstance(df, pd.DataFrame), "df is not a pandas dataframe."
     
     indices = _doi2int(doi)
     start = _ts2int(df.index[0])
